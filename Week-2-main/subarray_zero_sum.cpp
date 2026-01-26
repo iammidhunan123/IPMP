2fcf3a89e4e5e428b559a527aeccda7455a0c375
Midhunan A <amidhunan@gmail.com>
2026-01-26 23:02:05 +0530

    Create subarray_zero_sum.cpp

---
+++ Week-2-main/subarray_zero_sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Complete this function
+    // Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(vector<int>& arr) {
+        // Your code here
+        unordered_set<int> s;
+        int sum=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            sum+=arr[i];
+            if(sum==0)
+            {
+                return true;
+            }
+            if(s.find(sum)!=s.end())
+            {
+                return true;
+            }
+            s.insert(sum);
+        }
+        return false;
+    }
+};
