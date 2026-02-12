2c4c51ef4df7cb9c7a6770f473870d5e6e8aeb52
Midhunan A <amidhunan@gmail.com>
2026-02-12 15:07:59 +0530

    Create remove_duplicate_2.cpp

---
+++ Week-3-main/remove_duplicate_2.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        ListNode*dummy=new ListNode(-1);
+        ListNode*prev=dummy;
+        ListNode*curr=head;
+        dummy->next=head;
+        while(curr)
+        {
+            if(curr->next &&curr->val==curr->next->val)
+            {
+                int data=curr->val;
+                while(curr && curr->val==data)
+                {
+                    curr=curr->next;
+                }
+                prev->next=curr;
+
+            }
+            else
+            {
+                prev=curr;
+                curr=curr->next;
+            }
+        }
+        return dummy->next;
+
+        
+    }
+};
